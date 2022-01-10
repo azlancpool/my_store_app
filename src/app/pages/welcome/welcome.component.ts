83f809e19826c9857e3eb5d6381e179ea5820b07
Cristian Naranjo <crispool@hotmail.com>
2022-01-10 17:18:21 -0500

    Adding modal that allow to add a new product

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IProduct } from './models/product.model';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  newProductModalIsVisible = false;
+  validateForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.validateForm = this.fb.group({
+      name: ["", [Validators.required]],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  newProduct() {
+    console.log("new product pressed")
+    this.newProductModalIsVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.newProductModalIsVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.newProductModalIsVisible = false;
   }
 
 }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 import { WelcomeComponent } from './welcome.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 
 @NgModule({
   imports: [
     WelcomeRoutingModule,
     NzIconModule,
+    NzModalModule,
+    NzInputModule,
+    NzFormModule,
+    NzButtonModule,
+    ReactiveFormsModule,
   ],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
-export class WelcomeModule { }
+export class WelcomeModule {
+
+}
